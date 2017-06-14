4abfa85479489b227577397f1951ca386bb1699b
nopi96912 <nopiwork@gmail.com>
2017-06-14 13:29:01 +0200

    Part 1,2 and 3 working

--- gameInventory.py
+++ gameInventory.py
@@ -1,15 +1,28 @@
-# This is the file where you must work. Write code in the functions, create new functions, 
+import operator
+# This is the file where you must work. Write code in the functions, create new functions,
 # so they work according to the specification
-
+inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 # Displays the inventory.
 def display_inventory(inventory):
-    pass
+    print("Inventory:")
+    item_number = 0
+    for k, v in inventory.items():
+        print(str(v) + " " + str(k))
+        item_number += v
+    print("Total number of items: " + str(item_number))
 
+#display_inventory(inv)
 
 # Adds to the inventory dictionary a list of items from added_items.
 def add_to_inventory(inventory, added_items):
-    pass
+    for i in range(len(added_items)):
+        inventory.setdefault(added_items[i], 0)
+        inventory[added_items[i]] = inventory[added_items[i]] + 1
+    return inventory
 
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = add_to_inventory(inv, dragon_loot)
+#display_inventory(inv)
 
 # Takes your inventory and displays it in a well-organized table with 
 # each column right-justified. The input argument is an order parameter (string)
@@ -19,8 +32,24 @@ def add_to_inventory(inventory, added_items):
 #   in descending order
 # - "count,asc" means the table is ordered by count in ascending order
 def print_table(inventory, order=None):
-    pass
-
+    coloum_size = int(max(len(x) for x in inventory))
+    print('Inventory:' '\n'
+          'count:'.rjust(coloum_size) + 'item name:'.rjust(coloum_size *2))
+    if order == 'count,desc':
+        descending_inv = sorted(inventory.items(), key = operator.itemgetter(1), reverse=True)
+        for k, v in descending_inv:
+            print(str(v).rjust(coloum_size) + k.rjust(coloum_size *2))
+        print('Total number of items:', sum(inventory.values()))
+    elif order == 'count,asc':
+        ascending_inv = sorted(inventory.items(), key = operator.itemgetter(1))
+        for k, v in ascending_inv:
+            print(str(v).rjust(coloum_size) + k.rjust(coloum_size *2))
+        print('Total number of items:', sum(inventory.values()))
+    else:
+        for k, v in inventory.items():
+            print(str(v).rjust(coloum_size) + k.rjust(coloum_size *2))
+        print('Total number of items:', sum(inventory.values()))
+print_table(inv)
 
 # Imports new inventory items from a file
 # The filename comes as an argument, but by default it's 
